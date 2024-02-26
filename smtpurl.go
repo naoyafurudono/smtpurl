079d4a966f8addcb3e477fe967503547ba314b2f
Naoya Furudono <naoya-furudono@pepabo.com>
2024-02-26 19:47:05 +0900

    remove nesting branch

--- smtpurl.go
+++ smtpurl.go
@@ -48,13 +48,12 @@ func Parse(raw string) (string, smtp.Auth, error) {
 		default:
 			return "", nil, fmt.Errorf("unsupported auth method: %s", strings.ToUpper(su[1]))
 		}
-	} else {
-		if u.User == nil {
-			return host, auth, nil
-		}
+	} else if u.User != nil{
 		// PLAIN
 		pass, _ := u.User.Password()
 		auth = smtp.PlainAuth("", u.User.Username(), pass, hostname)
+	} else {
+		return host, auth, nil
 	}
 	return host, auth, nil
 }
